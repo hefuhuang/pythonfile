d560f9143eba668146ea3d4e0edb56df2f7febfb
xiaoy <1966423770@qq.com>
2017-04-27 09:45:00 +0800

    view_url

--- myblog/myblog/urls.py
+++ myblog/myblog/urls.py
@@ -15,7 +15,8 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
-
+import  blog.views as bv
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^index/', bv.index),
 ]
