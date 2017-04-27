38c5e333b3057ab9d8c9bc4650526856d3b8abb0
xiaoy <1966423770@qq.com>
2017-04-27 11:36:22 +0800

    templates

--- myblog/blog/views.py
+++ myblog/blog/views.py
@@ -4,6 +4,6 @@ from django.shortcuts import render
 from django.http import HttpResponse 
 
 def index(request):
- return HttpResponse('hello world');
+ return render(request,'index.html');
 
 # Create your views here.
